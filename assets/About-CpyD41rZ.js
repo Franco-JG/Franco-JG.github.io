13faae82c5e498c340d3ec73ab8954e653cee8a3
Franco-JG <jeftefranco@outlook.com>
2025-03-13 05:47:34 +0000

    layout v1.2

---
+++ assets/About-CpyD41rZ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./react-three-CwI-z3rJ.js";import{B as e}from"./BlurCard-GnI92nh0.js";import"./three-core-DRk8PRKa.js";const t=()=>r.jsx("div",{className:"relative-content",children:r.jsxs(e,{children:[r.jsx("h2",{children:"Sobre mí"}),r.jsx("p",{children:"Aquí puedes agregar información adicional sobre ti."})]})});export{t as default};
