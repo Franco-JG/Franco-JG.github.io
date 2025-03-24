ada4730c74fcd4b38707519f0261112fa07d02c2
Franco-JG <jeftefranco@outlook.com>
2025-03-23 22:03:38 -0600

    layout v5

--- assets/vendor-DcSoGzia.js
+++ assets/vendor-DcSoGzia.js
@@ -1,4 +1,4 @@
-import{r as O}from"./react-three-kig-QZXa.js";var y={exports:{}},n={};/**
+import{r as O}from"./react-three-SB0_oqID.js";var y={exports:{}},n={};/**
  * @license React
  * react-dom.production.js
  *
